88e4ab42af9cc262819ce34f3a0a895da9f57587
AndrewB92 <babujioh@gmail.com>
2026-01-12 11:43:35 +0200

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteMetadata = {
-  siteName: "Andrew Bielous",
+  siteName: "<Andrew_Dev/>",
   tagline: "Product-focused web developer",
   description:
     "Frontend-focused engineer building thoughtful WordPress & React experiences with a focus on performance and storytelling.",
