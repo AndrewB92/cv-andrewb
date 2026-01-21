efd793f7a72365cb4607e31b6a5d2a5a5146c0d4
AndrewB92 <babujioh@gmail.com>
2026-01-21 15:15:43 +0200

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,25 +21,24 @@ export default async function HomePage() {
         <div className={styles.heroContent}>
           <div className="styles.heroContentBG"></div>
           <p aria-label="Tagline">Hello there and welkome to my website</p>
-          <h1>I&apos;m {profile.name},a 
-            <span className="magic">
-              <span className="magic-star">
+          <h1 className={styles.title}>I&apos;m {profile.name},a&nbsp;
+            <span className={styles.magic}>
+              <span className={styles.magicStar}>
                   <svg viewBox="0 0 512 512">
                   <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
                   </svg>
                 </span>
-              <span className="magic-star">
+              <span className={styles.magicStar}>
                   <svg viewBox="0 0 512 512">
                   <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
                   </svg>
                 </span>
-              <span className="magic-star">
+              <span className={styles.magicStar}>
                   <svg viewBox="0 0 512 512">
                   <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
                   </svg>
                 </span>
-              <span className="magic-text">
-                        product focused</span>
+              <span className={styles.magicText}>product focused</span>
             </span> web developer
           </h1>
 
